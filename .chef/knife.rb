d95c879c864f33da947acb8bb7bf6e1a393b6d95
jtimberman <joshua@opscode.com>
2013-01-31 00:50:30 -0700

    allow local knife config options
    If you wish to modify the knife configuration, e.g. AWS options for
    `knife ec2` or Chef Server URL/keys, put them in
    `.chef/knife.local.rb` and it will be loaded automatically.

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -4,4 +4,8 @@ node_name     ENV["NODE_NAME"] || "solo"
 client_key    File.expand_path("../solo.pem", __FILE__)
 cache_type    "BasicFile"
 cache_options(path: File.expand_path("../checksums", __FILE__))
-cookbook_path [ File.expand_path("../../cookbooks", __FILE__) ]
+cookbook_path [ File.expand_path("../../chef/cookbooks", __FILE__) ]
+# Allow users to add or override knife locally
+if ::File.exist?(File.expand_path("../knife.local.rb", __FILE__))
+  Chef::Config.from_file(File.expand_path("../knife.local.rb", __FILE__))
+end
