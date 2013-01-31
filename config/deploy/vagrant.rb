780a99cdf63937b160a631b41533ed21498a80d7
Steven Danna <steve@opscode.com>
2013-01-31 03:20:20 -0800

    Multi-node vagrant config.

--- config/deploy/vagrant.rb
+++ config/deploy/vagrant.rb
@@ -8,3 +8,12 @@ set :node_name, "vagrant-app"
 
 set :chef_binary, "/usr/bin/chef-solo"
 
+server "33.33.33.11", :lb
+set :server_ip, "33.33.33.11"
+set :user, 'vagrant'
+set :id_file, "#{ENV['HOME']}/.vagrant.d/insecure_private_key"
+ssh_options[:keys] = [id_file]
+set :environment_name, "vagrant"
+set :node_name, "vagrant-lb"
+
+set :chef_binary, "/usr/bin/chef-solo"
