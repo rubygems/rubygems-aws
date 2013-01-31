dfd95a2d5197492e8096740e463a8411239a65fa
Bryan McLellan <btm@opscode.com>
2013-01-30 19:20:20 -0800

    remove undesired bashrc cookbook

--- roles/shell.rb
+++ roles/shell.rb
@@ -2,7 +2,6 @@ name "shell"
 description "The role with shell-related recipes for nodes"
 run_list(
   "recipe[bash-completion]",
-  "recipe[bashrc]",
   "recipe[grc]",
   "recipe[screen]",
   "recipe[rvm::system]"
