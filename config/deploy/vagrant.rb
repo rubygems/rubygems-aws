67689e80c8d66e82cce2193d2040cce718ecfd55
Luke Chadwick <luke.a.chadwick@gmail.com>
2013-01-31 23:12:26 +1100

    Support passing roles through capistrano (rough)

--- config/deploy/vagrant.rb
+++ config/deploy/vagrant.rb
@@ -1,19 +1,10 @@
 server "33.33.33.10", :app, :web, :db
-set :server_ip, "33.33.33.10"
-set :user, 'vagrant'
-set :id_file, "#{ENV['HOME']}/.vagrant.d/insecure_private_key"
-ssh_options[:keys] = [id_file]
-set :environment_name, "vagrant"
-set :node_name, "vagrant-app"
-
-set :chef_binary, "/usr/bin/chef-solo"
-
 server "33.33.33.11", :lb
-set :server_ip, "33.33.33.11"
+#set :server_ip, "33.33.33.10"
 set :user, 'vagrant'
 set :id_file, "#{ENV['HOME']}/.vagrant.d/insecure_private_key"
 ssh_options[:keys] = [id_file]
 set :environment_name, "vagrant"
-set :node_name, "vagrant-lb"
+set :node_name, "vagrant"
 
 set :chef_binary, "/usr/bin/chef-solo"
