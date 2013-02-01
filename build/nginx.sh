6e9c61dec9fd7e8db0f37b08b5920e78db1635e9
Adam Jacob <adam@opscode.com>
2013-02-01 11:26:07 -0800

    Adding stub_status, gzip_statis and realip nginx modules

--- build/nginx.sh
+++ build/nginx.sh
@@ -63,7 +63,10 @@ cd $TMP_DIR/nginx-$NGINX_VERSION
 ./configure --prefix=/opt/nginx \
   --with-http_ssl_module \
   --with-debug \
-  --with-http_geoip_module
+  --with-http_geoip_module \
+  --with-http_stub_status_module \
+  --with-http_gzip_static_module \
+  --with-http_realip_module
 make
 mkdir -p $TMP_DIR/$NGINX_PREFIX
 env DESTDIR=$TMP_DIR make install
