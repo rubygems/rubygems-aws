52d97a8c25f64af2c0641d2624d58bf9479a44a2
Steven Danna <steve@opscode.com>
2013-02-01 09:15:12 -0800

    Use prebuilt nginx package with geoip.
    This commit adds a pre-built nginx debian package to the repository
    and installs nginx using that package.  Ideally, this package would be
    stored on an asset server or apt repository.
    
    Despite the loss of history, we might consider a git-filter-branch
    after we are able to remove this asset from the repo.

--- chef/roles/balancer.rb
+++ chef/roles/balancer.rb
@@ -2,13 +2,14 @@ name "balancer"
 description "The role which contains all cookbooks for a load balancer."
 run_list(
   "role[base]",
-  "recipe[nginx::server]",
+  "recipe[rubygems::nginx_source]",
   "recipe[rubygems::balancer]"
 )
 
 override_attributes(
   "application" => { "application_servers" => [ "10.249.31.114" ]},
   "nginx" => {
-    "geoip" => false
+    "geoip" => true,
+    "dir" => "/opt/nginx/conf"
   }
 )
---
+++ chef/site-cookbooks/rubygems/recipes/nginx_source.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: nginx
+# Recipe:: nginx_source.rb
+#
+
+package_name = "nginx_#{node['nginx']['version']}-1_amd64.deb"
+
+cookbook_file "#{Chef::Config[:file_cache_path]}/#{package_name}"
+
+dpkg_package "#{Chef::Config[:file_cache_path]}/#{package_name}"
+
+# Continue to use upstream cookbooks
+# for basic configuration and service management
+
+template "/etc/init.d/nginx" do
+  source "nginx.init.erb"
+  owner "root"
+  group "root"
+  mode 00755
+  variables({:src_binary => "/opt/nginx/sbin/nginx",
+             :pid => node['nginx']['pid']
+            })
+end
+
+directory "/opt/nginx/conf/conf.d"
+directory "/var/log/nginx"
+
+include_recipe "nginx::service"
+include_recipe "nginx::configuration"
