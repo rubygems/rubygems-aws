1f8f2b7cbcacbfd78f1a3be371a3f28c71b03757
David Radcliffe <radcliffe.david@gmail.com>
2013-10-17 18:10:05 -0400

    ensure /etc/cloud exists

--- chef/site-cookbooks/rubygems/recipes/cloudinit.rb
+++ chef/site-cookbooks/rubygems/recipes/cloudinit.rb
@@ -1,3 +1,15 @@
+#
+# Cookbook Name:: rubygems
+# Recipe:: cloudinit
+#
+
+directory "/etc/cloud" do
+  owner "root"
+  group "root"
+  mode "0755"
+  action :create
+end
+
 template "/etc/cloud/cloud.cfg" do
   source "cloudinit.cfg.erb"
   owner "root"
