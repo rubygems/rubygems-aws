50017f5c7c4e721227bf1857f0cd2b97b02c7ae9
David Radcliffe <radcliffe.david@gmail.com>
2013-10-21 10:14:21 -0400

    cleanup chef metadata

--- chef/site-cookbooks/rubygems/metadata.rb
+++ chef/site-cookbooks/rubygems/metadata.rb
@@ -1,13 +1,34 @@
-maintainer       "Phil Cohen"
-maintainer_email "github@phlippers.net"
-license          "All rights reserved"
-description      "RubyGems-specific recipes"
-long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "0.1.0"
+name             'rubygems'
+maintainer       'RubyGems.org Ops Team'
+license          'MIT'
+description      'RubyGems.org specific recipes'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
 
-recipe "rubygems", "Default recipes for application deployment nodes"
-recipe "rubygems::users", "User setup and configuration"
+recipe 'rubygems',                        'Default recipes for all nodes'
+recipe 'rubygems::balancer',              'Nginx load balancer config'
+recipe 'rubygems::cloudinit',             'Ubuntu cloud config'
+recipe 'rubygems::datadog',               'Installs and configures datadog'
+recipe 'rubygems::debug-tools',           'Installs common debug packages'
+recipe 'rubygems::environment_variables', 'Configure node env variables'
+recipe 'rubygems::ip_security',           'System security settings'
+recipe 'rubygems::jenkins',               'Configure extra jenkins details'
+recipe 'rubygems::monit',                 'Configure monit logs and monitors'
+recipe 'rubygems::nginx_source',          'Installs nginx from our pre-build package'
+recipe 'rubygems::rails',                 'Main app recipe'
+recipe 'rubygems::rails_nginx',           'nginx config for app server'
+recipe 'rubygems::stat-update',           'Installs stat-update from pre-built package'
+recipe 'rubygems::system_ruby',           'Installs ruby'
+recipe 'rubygems::users',                 'User setup and configuration'
 
-depends "user"
-depends "application"
-depends "application_ruby"
+depends 'user'
+depends 'application'
+depends 'application_ruby'
+depends 'logrotate'
+depends 'datadog'
+depends 'nginx'
+depends 'rbenv'
+depends 'monit'
+depends 'runit'
+
+supports 'ubuntu'
