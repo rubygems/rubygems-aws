36ded115ac9f74bed4a7ca404d947dd6e625785a
David Radcliffe <radcliffe.david@gmail.com>
2014-01-21 08:40:03 -0500

    only site-cookbooks should be in the path, since knife-solo adds Berkshelf cookbooks automatically

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,5 +1,5 @@
 # knife[:provisioning_path] = '/var/cache/chef'
 
-cookbook_path   ['chef/cookbooks', 'chef/site-cookbooks']
+cookbook_path   ['chef/site-cookbooks']
 role_path       'chef/roles'
 data_bag_path   'chef/data_bags'
