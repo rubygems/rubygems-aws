7d9db8ac7f5ca0297939ea941749da8a6e54b273
Sam Kottler <shk@redhat.com>
2014-03-23 20:48:32 +0600

    Add the updated knife configuration for using hosted chef

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,5 +1,11 @@
-# knife[:provisioning_path] = '/var/cache/chef'
-
-cookbook_path   []
-role_path       'chef/roles'
-data_bag_path   'chef/data_bags'
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                ENV['RUBYGEMS_CHEF_USERNAME']
+client_key               "#{current_dir}/#{ENV['RUBYGEMS_CHEF_USERNAME']}.pem"
+validation_client_name   "rubygems-validator"
+validation_key           "#{current_dir}/rubygems-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/rubygems"
+cache_type               'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
