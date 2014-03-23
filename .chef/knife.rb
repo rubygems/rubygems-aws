2c041300eeb63915db2b99f6490fd76604b8b139
Sam Kottler <shk@redhat.com>
2014-03-24 00:20:55 +0600

    Raise unless the proper environment variable is set

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,8 +1,12 @@
+chef_username = 'RUBYGEMS_CHEF_USERNAME'
+env_username = ENV[chef_username]
+raise "You need to set the environment variable #{chef_username} to your hosted chef username" unless env_username
+
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                ENV['RUBYGEMS_CHEF_USERNAME']
-client_key               "#{current_dir}/#{ENV['RUBYGEMS_CHEF_USERNAME']}.pem"
+node_name                chef_username
+client_key               "#{current_dir}/#{env_username}.pem"
 validation_client_name   "rubygems-validator"
 validation_key           "#{current_dir}/rubygems-validator.pem"
 chef_server_url          "https://api.opscode.com/organizations/rubygems"
